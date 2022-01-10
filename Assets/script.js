584d2e80e72572c02e8f065737a36c6c76ef9517
bruno-biancalana <bruno.biancalana@outlook.com>
2022-01-09 23:13:44 -0300

    Inclui laço condicional para operações (js)

--- Assets/script.js
+++ Assets/script.js
@@ -2,4 +2,29 @@ function calculadora () {
     const operacao = prompt ("Escolha uma operação:\n1-soma (+)\n2-subtração(-)\n3-multiplicação(*)\n4-divisão real(/)\n5-divisão inteira(%)\n6-potenciação(^^)")
 }
 
+if (operacao ==1 ){
+    soma()
+} else if (operacao ==2)
+    {
+        subtracao();
+    }
+        else if (operacao ==3)
+        {
+            multiplicacao();
+        }
+            else if (operacao ==4)
+            {
+                 divisaoReal();
+            }
+                else if (operacao ==2)
+                {
+                    divisaoInteira();
+                }
+                    else (operacao ==2)
+                    {
+                        potenciacao();
+                     }
+
+
+
 calculadora ();
