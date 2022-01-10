a6d272c734c7b275a447ff83f51261445de1784c
bruno-biancalana <bruno.biancalana@outlook.com>
2022-01-09 23:08:09 -0300

    Adiciona a função calculadora

---
+++ Assets/script.js
@@ -0,0 +1,5 @@
+function calculadora () {
+    const operacao = prompt ("Escolha uma operação:\n1-soma (+)\n2-subtração(-)\n3-multiplicação(*)\n4-divisão real(/)\n5-divisão inteira(%)\n6-potenciação(^^)")
+}
+
+calculadora ();
