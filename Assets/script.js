0b5be2e1ffd2babf7cd56ab6c1e1e1084e18ac7f
bruno-biancalana <bruno.biancalana@outlook.com>
2022-01-09 23:15:53 -0300

    Criação de variáveis dos valores que serão digitados pelo usuário

--- Assets/script.js
+++ Assets/script.js
@@ -2,6 +2,10 @@ function calculadora () {
     const operacao = prompt ("Escolha uma operação:\n1-soma (+)\n2-subtração(-)\n3-multiplicação(*)\n4-divisão real(/)\n5-divisão inteira(%)\n6-potenciação(^^)")
 }
 
+let n1 = prompt("Insira o primeiro valor: ");
+let n2 = prompt("Insira o segundo valor: ");
+let resultado; 
+
 if (operacao ==1 ){
     soma()
 } else if (operacao ==2)
